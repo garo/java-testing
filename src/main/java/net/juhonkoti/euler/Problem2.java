852e2ddc6415a1b83b7214f725b37188dd34431c
Juho Mäkinen <juho.makinen@gmail.com>
2018-01-23 16:51:21 +0200

    Solved euler problem 2

---
+++ src/main/java/net/juhonkoti/euler/Problem2.java
@@ -0,0 +1,41 @@
+package net.juhonkoti.euler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem2 {
+
+    public static List<Integer> fibbonacci(int count) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>(count);
+
+        int t1 = 1, t2 = 2;
+        numbers.add(1);
+        numbers.add(2);
+        for (int i = 2; i < count; i++) {
+            numbers.add(t1 + t2);
+            int tmp = t2;
+            t2 = t1 + t2;
+            t1 = tmp;
+        }
+
+        return numbers;
+    }
+
+    // https://projecteuler.net/problem=2
+    public static int eulerQuestion(int max) {
+        int sum = 2;
+        int t1 = 1, t2 = 2;
+        int m;
+        do {
+            m = t1 + t2;
+            t1 = t2;
+            t2 = m;
+
+            if (m % 2 == 0) {
+                sum += m;
+            }
+        } while (m < max);
+
+        return sum;
+    }
+}
---
+++ src/test/java/net/juhonkoti/euler/Problem2Test.java
@@ -0,0 +1,33 @@
+package net.juhonkoti.euler;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class Problem2Test {
+
+    @Test
+    public void testExampleInputWorks() {
+
+        List<Integer> result = Problem2.fibbonacci(10);
+        assertThat(result, CoreMatchers.hasItems(1,2,3,5,8,13,21,34,55,89));
+
+    }
+
+    @Test
+    public void testEulerQuestionWorks() {
+        int sum = Problem2.eulerQuestion(50);
+        assertEquals(sum, 2+8+34);
+    }
+
+
+    @Test
+    public void printEulerQuestion() {
+        System.out.println("Euler question 2 answer: " + Problem2.eulerQuestion(4000000));
+    }
+}
